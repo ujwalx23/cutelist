c0cc0d8b7a5e32611f2285b2204fd9b0cff0673f
UJWAL SINGH <ujwalsingh164@gmail.com>
2025-05-03 14:32:23 +0530

    Update Books.tsx

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -200,7 +200,7 @@ const Books = () => {
                 My Books
               </h1>
               <p className="text-center text-gray-400 mb-6">
-                Keep track of your reading list and achievements
+                Keep track of your reading lists 
               </p>
 
               {user ? (
